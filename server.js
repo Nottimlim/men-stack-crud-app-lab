b82a10816e2ad656f9af23d03e17ccb8ea49a777
Timothy Lim <contacttimothylim@gmail.com>
2024-09-26 15:26:45 -0400

    commit

--- server.js
+++ server.js
@@ -1,14 +1,33 @@
+// Routes
 const express = require('express');
 const mongoose = require('mongoose');
+const Manga = require('./models/Manga.js');
 const app = express();
 const port = process.env.PORT || 3000;
 const dotenv = require("dotenv");
 dotenv.config();
 
+// Connect
+mongoose.connect(process.env.MONGODB_URI);
+mongoose.connection.on("connected", () => {
+    console.log(`Connected to MongoDB ${mongoose.connection.name}`);
+})
 
-app.set('view engine', 'ejs');
+// Middleware
+app.use(express.urlencoded({ extended: true }));
 
-app.get('/test', (req, res) => {
+app.get('/manga/new.ejs', (req, res) => {
+  res.render('newManga.ejs');
+});
+
+app.post('/manga', async (req, res) => {
+  const { title, genre, chapters, rating, description, imageUrl } = req.body;
+  const newManga = new Manga({ title, genre, chapters, rating, description, imageUrl });
+  await newManga.save();
+  res.redirect('/manga');
+});
+
+app.get('/', (req, res) => {
   res.send('Server is running');
 });
 
